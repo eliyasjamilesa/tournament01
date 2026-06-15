3e519a0a45fb2b31c7bc8688ac97c1624ee4e61c
Eliyas Jamil Esa <eliyasjamil909@gmail.com>
2026-06-15 13:42:48 +0000

    Add my app in this workspace

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAs-placeholder-please-replace-with-real-key",
-  authDomain: "studio-4193591705-75870.firebaseapp.com",
-  projectId: "studio-4193591705-75870",
-  storageBucket: "studio-4193591705-75870.appspot.com",
+  authDomain: "tournament-app-76d39.firebaseapp.com",
+  projectId: "tournament-app-76d39",
+  storageBucket: "tournament-app-76d39.appspot.com",
   messagingSenderId: "75870",
   appId: "1:75870:web:placeholder-id"
 };
