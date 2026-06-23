6300bfbe956146ea29eb0705ef4305a45fcc9c20
Eliyas Jamil Esa <eliyasjamil909@gmail.com>
2026-06-23 06:22:51 +0000

    I am building a Next.js app with Capacitor and exporting it as an Androi

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,9 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tstour.app',
   appName: 'Ts Tour',
   webDir: 'out',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
   }
